ea4208058ed50502b941e5839ec854221d0cb930
Chatchai Honarongsiri <soulquiza@gmail.com>
2018-05-12 00:39:31 +0700

    change server port to 8080

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var port = 8081;
+var port = 8080;
 
 var server = app.listen(port, function() {
     console.log('Listening on port: ' + port);
